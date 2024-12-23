2fe1ecd01830df7235f186f3c3e9e71d38f3e3dc
Ankon Chowdhury <ankonchy1@gmail.com>
2024-12-23 14:18:24 +0600

    login,register completed successfully

--- src/routes/Router.jsx
+++ src/routes/Router.jsx
@@ -1,10 +1,25 @@
 import { createBrowserRouter } from "react-router-dom";
 import MainLayout from "../layouts/MainLayout/MainLayout";
+import Register from "../pages/Register/Register";
+import Login from "../pages/Login/Login";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <MainLayout></MainLayout>,
+    children: [
+      {
+        path: "/",
+      },
+      {
+        path: "/register",
+        element: <Register></Register>,
+      },
+      {
+        path: "/login",
+        element: <Login></Login>,
+      },
+    ],
   },
 ]);
 
